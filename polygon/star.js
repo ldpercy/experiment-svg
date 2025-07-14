fabeafb1c74c9bef3ad6ee7775f9bb9649b6f99b
Paul Dale <ldpercy@gmail.com>
2025-07-14 12:56:11 +1000

    Add star copies at an offset

--- polygon/star.js
+++ polygon/star.js
@@ -3,23 +3,33 @@
 
 function bodyOnload() {
 
-	drawStar()
+	redraw();
 }
 
 
-function drawStar(){
+function redraw(){
 
 	const sides = document.getElementById('input-sides').value;
-	const offset = document.getElementById('input-offset').value;
+	const pointStep = document.getElementById('input-pointStep').value;
 	const startDivision = document.getElementById('input-startDivision').value;
 
-	let starPath = getStarPath(sides, offset, startDivision);
+
+	const copies = document.getElementById('input-copies').value;
+	const divisionOffset = document.getElementById('input-divisionOffset').value;
+
+	const starGroup = getStarPath(sides, pointStep, startDivision, copies, divisionOffset);
+
 	//console.log(starPath);
-	document.getElementById('star').setAttribute('d',starPath);
+	document.getElementById('star-group').innerHTML = starGroup;
 }
 
+/*
+function drawStar(){
 
-function getStarPath(sides, offset, startDivision) {
+} */
+
+
+function getStarPath(sides, pointStep, startDivision, copies, divisionOffset) {
 	const length = 1000;
 	let path = '';
 	let x = 0, y = 0;
@@ -30,18 +40,23 @@ function getStarPath(sides, offset, startDivision) {
 
 	let pointRadians = startAngle;
 
-	for (let i=0; i < sides; i++)
-	{
-		pointRadians += offset * mainAngle;
+	for (let c=0; c < copies; c++) {
 
-		const p = polarPoint(pointRadians, length)
+		pointRadians += mainAngle/divisionOffset;
 
-		x = Math.round(p.x);
-		y = Math.round(p.y);
-		//console.log(i,Math.round(degrees(pointRadians)),x,y);
+		for (let i=0; i < sides; i++)
+		{
+			pointRadians += mainAngle * pointStep;
 
-		path  += (i===0) ? `M ${x} ${y} ` : `L ${x} ${y} `;
+			const p = polarPoint(pointRadians, length)
+
+			x = Math.round(p.x);
+			y = Math.round(p.y);
+			//console.log(i,Math.round(degrees(pointRadians)),x,y);
+
+			path  += (i===0) ? `M ${x} ${y} ` : `L ${x} ${y} `;
+		}
 	}
 
-	return `${path} Z`;
+	return `<path class="star" d="${path} Z"/>`;
 }
