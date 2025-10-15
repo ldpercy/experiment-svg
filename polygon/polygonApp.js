88c3760e976fe812204493fed4c2f8b67d6da6de
Paul Dale <ldpercy@gmail.com>
2025-10-15 20:25:50 +1100

    Squashed commit of the following:
    commit cf954b8a81f53d55ba49752e2467f6c9e419dd82
    Author: Paul Dale <ldpercy@gmail.com>
    Date:   Sun Oct 12 15:59:59 2025 +1100
    
        Update for HTMLApp
    
    commit 31115520e3bba876294e8cd29dcc716825546d35
    Author: Paul Dale <ldpercy@gmail.com>
    Date:   Sun Oct 12 15:08:42 2025 +1100
    
        Seems to be working now, still messy though
    
    commit 614acc9eee818318f70aee1aa96ca1f85dd64a6f
    Author: Paul Dale <ldpercy@gmail.com>
    Date:   Sun Oct 12 14:38:49 2025 +1100
    
        Move functions in to become methods
    
    commit 5e553ee76933d3123d8a59b8d5238bd429fe245a
    Author: Paul Dale <ldpercy@gmail.com>
    Date:   Sat Oct 11 19:42:00 2025 +1100
    
        trying htmlapp

---
+++ polygon/polygonApp.js
@@ -0,0 +1,184 @@
+//
+//	polygonApp.js
+//
+
+
+class PolygonApp extends HTMLApp {
+
+	name = "polygonApp";
+	info = "Polygon by ldpercy"
+
+	eventListeners = [
+		{
+			query: '#form-polygon',
+			type: 'change',
+			listener: ()=>this.polygonChangeListener
+		},
+		{
+			query: '#form-style',
+			type: 'change',
+			listener: ()=>this.styleChangeListener
+		},
+	];
+
+
+	polygonChangeListener(event) {
+		this.redraw();
+	}
+
+	styleChangeListener(event) {
+		this.updateStyle();
+	}
+
+
+
+	/*	listener: ()=>this.listenerFunction
+
+	I did the same thing in yearclock for the dynamic hand functions.
+	I'm not sure if it needs to be like this, but writing it directly as:
+
+		listener: this.listenerFunction
+
+	wasn't working for me.
+	Like this there's an extra evaluation step also.
+	Would like to find out what's going on here, and if there's a better way.
+
+	*/
+
+
+
+	documentDOMContentLoaded() {
+		super.documentDOMContentLoaded();
+		this.redraw();
+	}/* documentDOMContentLoaded */
+
+
+
+
+
+	redraw() {
+		console.debug('polygonApp.redraw', arguments);
+
+		const radius = document.getElementById('input-radius').value;
+		const sides = document.getElementById('input-sides').value;
+		const pointStep = document.getElementById('input-pointStep').value;
+		const startDivision = document.getElementById('input-startDivision').value;
+
+		const copies = document.getElementById('input-copies').value;
+		const divisionOffset = document.getElementById('input-divisionOffset').value;
+		const separatePaths = document.getElementById('input-copyPaths').value;
+		const coordinates = document.getElementById('input-coordinates').value;
+
+		const starGroup = this.getStarPath(radius, sides, pointStep, startDivision, copies, divisionOffset, separatePaths, coordinates);
+
+		//console.log(starPath);
+		document.getElementById('star-group').innerHTML = starGroup;
+	}/* redraw */
+
+
+
+
+	updateStyle() {
+
+		console.debug('polygonApp.updateStyle');
+
+		if (document.getElementById('input-fillRule').value === 'evenodd') {
+			document.getElementById('star-group').classList.add('evenodd');
+		}
+		else {
+			document.getElementById('star-group').classList.remove('evenodd');
+		}
+
+		if (document.getElementById('input-showMarkers').checked) {
+			document.getElementById('star-group').classList.add('show-markers');
+		}
+		else {
+			document.getElementById('star-group').classList.remove('show-markers');
+		}
+
+		if (document.getElementById('input-showGrid').checked) {
+			document.getElementById('group-grid').style.display = '';
+		}
+		else {
+			document.getElementById('group-grid').style.display = 'none';
+		}
+
+	}/* updateStyle */
+
+
+
+	/* getStarPath
+	*/
+	getStarPath(
+			radius,				// radius of points
+			sides, 				// how many sides the polygon has
+			pointStep, 			// how many divisions to the next vertex
+			startDivision,		// integer divisions of the base angle to the start of the polygon
+			copies, 			// number of copies of the polygon to draw
+			divisionOffset,		// integer divisions of the base angle between the copies
+			copyPaths,			// combined or separate svg paths
+			coordinates,		// absolute or relative
+		) {
+		let result = '';
+		let path = '';
+		let x = 0, y = 0;
+
+		const mainAngle = Math.TAU / sides;
+
+		const startAngle = mainAngle/startDivision;
+
+		let pointRadians = startAngle;
+		let lastPoint = new Point(0,0);
+		for (let c=0; c < copies; c++) {
+
+			pointRadians += mainAngle/divisionOffset;
+
+
+			for (let i=0; i <= sides; i++)
+			{
+				pointRadians += mainAngle * pointStep;
+
+				const p = new PolarPoint(pointRadians, radius).toPoint();
+
+				if (coordinates === 'relative') {
+
+					x = Math.round(p.x - lastPoint.x);
+					y = Math.round(p.y - lastPoint.y);
+
+					path  += (i===0) ? `m${x},${y} ` : `l${x},${y} `;
+					lastPoint = p;
+
+				} else {
+					x = Math.round(p.x);
+					y = Math.round(p.y);
+					//console.log(i,Math.round(degrees(pointRadians)),x,y);
+
+					path  += (i===0) ? `M${x},${y} ` : `L${x},${y} `;
+				}
+			}// for i
+
+			if (copyPaths === 'separate') {
+				result += `<path class="star" d="${path} z"/>`;
+				lastPoint = new Point(0,0);
+				path = '';
+			}
+			else {
+				path += ` Z `;
+			}
+		}// for c
+
+		if (copyPaths === 'combined') {
+			result = `<path class="star" d="${path} z"/>`;
+		}
+
+		return result;
+	}/* getStarPath */
+
+
+
+}/* PolygonApp */
+
+
+
+polygonApp = new PolygonApp();
+
