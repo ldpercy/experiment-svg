0f648b5df1659e13fd3c25bbc80f046cd503c2b8
Paul Dale <ldpercy@gmail.com>
2025-12-17 11:39:51 +1100

    Bring in the web component experiment from html to start working with

--- [library]/HTMLApp.module.js
+++ [library]/HTMLApp.module.js
@@ -1,39 +1,51 @@
 /* HTMLApp
 */
-class HTMLApp {
+export class HTMLApp {
 
-	//name = 'HTMLApp';
-	//info = 'HTMLApp by ldpercy';
+	name = 'HTMLApp';
+	info = 'HTMLApp by ldpercy';
+
+	eventListeners = [
+		/* Example instance setup:
+		{
+			query: '#myForm',
+			type: 'change',
+			listener: ()=>this.formChangeListener
+		},
+		{
+			query: '#myButton',
+			type: 'click',
+			listener: ()=>this.buttonClickListener
+		},
+		*/
+	];
+
 
 	constructor() {
-		//console.debug('HTMLApp.constructor', this);
 		document.addEventListener('DOMContentLoaded', this.documentDOMContentLoaded.bind(this));
 		// adding a `.bind(this)` to the addEventListener gives the listener the instance 'this'
 	}
 
 
 	documentDOMContentLoaded() {
-		// by default event listeners like these receive HTMLDocument as 'this'
-		// and the event object as argument 0
-		// HTMLDocument doesn't seem all that useful as a 'this', especially in a class context
-		// adding a `.bind(this)` to the addEventListener keeps 'this' as the instance scope
-
-		//console.log('documentDOMContentLoaded', arguments, this);
-		this.addEventListeners();
 		console.info(this.info);
-	}/* documentDOMContentLoaded */
+		this.addEventListeners();
+	}
 
 
-
+	/* addEventListeners
+	*/
 	addEventListeners() {
 		this.eventListeners.forEach(
 			(item) => {
+				//this.addEventListener(item.query, item.event, item.listener);
 				document.querySelectorAll(item.query).forEach((node) => {
+					//console.debug('HTMLApp.addEventListeners item.listener', item.listener);
 					node.addEventListener(
 						item.type,
 						item.listener.bind(this)
 					);//addEventListener
-				});
+				}); //
 			}//item
 		);
 	}/* addEventListeners */
@@ -46,4 +58,9 @@ class HTMLApp {
 		return (new URL(window.location.href)).searchParams.get(name);
 	}
 
+
+
+
 }/* HTMLApp */
+
+
---
+++ web-component/index.js
@@ -0,0 +1,127 @@
+/* index.js
+*/
+
+import { HTMLApp } from "../[library]/HTMLApp.module.js";
+
+
+class IndexApp extends HTMLApp {
+	name = "Experiment: Web Component";
+	info = "Experiment: Web Component";
+
+	eventListeners = [
+		{
+			query: '#button-lightMode',
+			type: 'click',
+			listener: this.modeClickListener
+		},
+		{
+			query: '#button-darkMode',
+			type: 'click',
+			listener: this.modeClickListener
+		},
+		/* {
+			query: '#myForm',
+			type: 'change',
+			listener: this.formChangeListener
+		}, */
+	];
+
+
+
+
+	modeClickListener(event) {
+		//console.log(event.target.attributes); //attributes['value']
+		const newMode = event.target.attributes['mode'].value;
+		this.setMode(newMode);
+	}
+
+
+	setMode(mode) {
+		//console.log('setMode', mode);
+		if (mode === 'light')
+		{
+			document.documentElement.classList.replace('dark','light') ;
+		}
+		else {
+			document.documentElement.classList.replace('light','dark') ;
+		}
+		localStorage.mode = mode;
+	}
+
+
+	documentDOMContentLoaded() {
+		super.documentDOMContentLoaded();
+		this.setMode(localStorage.mode);
+
+
+		/*
+		const url = window.location;
+		document.getElementById('input-url').value = url;
+		const urlObj = new URL(url);
+		console.log(urlObj);
+		*/
+
+
+		// console.log('document.styleSheets:', document.styleSheets);
+
+
+	}
+
+
+}/* IndexApp */
+
+
+
+var indexApp = new IndexApp();
+
+
+
+// Create a class for the element
+class MyCustomElement extends HTMLElement {
+	static observedAttributes = ["color", "size"];
+
+	constructor() {
+		// Always call super first in constructor
+		super();
+
+		/*
+		let template = document.getElementById("template0");
+		let templateContent = template.content;
+		*/
+
+		/** @type { HTMLTemplateElement } */
+		let template = document.querySelector("#template0");
+		let templateContent = template.content;
+
+		const shadowRoot = this.attachShadow({ mode: "open" });
+		shadowRoot.appendChild(templateContent.cloneNode(true));
+
+	}
+
+	connectedCallback() {
+		console.log("Custom element added to page.");
+	}
+
+	disconnectedCallback() {
+		console.log("Custom element removed from page.");
+	}
+
+	connectedMoveCallback() {
+		console.log("Custom element moved with moveBefore()");
+	}
+
+	adoptedCallback() {
+		console.log("Custom element moved to new page.");
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		console.log(`Attribute ${name} has changed.`);
+	}
+}
+
+customElements.define("my-custom-element", MyCustomElement);
+
+
+
+
+
