ef9104ab3c7753e0c063eadd7a0025560815cf2a
Paul Dale <ldpercy@gmail.com>
2025-12-21 14:17:36 +1100

    Quick version of the worm that I used to code as a kid

--- [library]/maths.js
+++ [library]/maths.js
@@ -95,6 +95,23 @@ class Maths{
 		return result;
 	}
 
+	//
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+	//
+
+	static getRandomInt(min, max) {
+		const minCeiled = Math.ceil(min);
+		const maxFloored = Math.floor(max);
+		return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+	}
+
+	static getRandomIntInclusive(min, max) {
+		const minCeiled = Math.ceil(min);
+		const maxFloored = Math.floor(max);
+		return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+	}
+
+
 
 }/* Maths */
 
